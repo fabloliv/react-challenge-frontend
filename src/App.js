5eabe94fbd9a5c966d5f0bda0f88eb2b8fd08027
Fabrício Oliveira <fabriciololiveira@gmail.com>
2019-10-28 21:13:03 -0300

    Add Axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api'
+
 import './App.css';
 
 function App() {
+
+  const [] = useState();
+
+  function adicionarFuncionario () {
+    // ir para form cadastro
+    console.log("Cadastrar novo");
+  }
+  function listarAtivos () {
+    // listar funcionarios ativos
+    console.log("Listar Ativos");
+  }
+  function limparFiltros () {
+    // exibir todos os funcionarios
+    console.log("Mostrar todos");
+  }
+
   return (
-    <h1>hello</h1>
+    <>
+    <h1>Dashboard</h1>
+    
+    <button onClick={adicionarFuncionario}>+ Adicionar funcionário</button>
+
+    <button onClick={listarAtivos}>Ver apenas ativos</button>
+    <button onClick={limparFiltros}>Limpar Filtros</button>
+    </>
   );
 }
 
