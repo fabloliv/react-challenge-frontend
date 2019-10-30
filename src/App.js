a1f0d0c40c41e5172e91f0d72d11d798cf6d57e9
Fabrício Oliveira <fabriciololiveira@gmail.com>
2019-10-29 22:17:44 -0300

    Create list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
-import React, { useState } from 'react';
-import api from './services/api'
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
 
 function App() {
 
-  const [] = useState();
+  const [data, setData] = useState([]);
 
-  function adicionarFuncionario () {
+  useEffect(async () => {
+    const result = await api.get('/funcionarios')
+    setData(result.data)
+  }, []);
+
+  function adicionarFuncionario() {
     // ir para form cadastro
     console.log("Cadastrar novo");
   }
-  function listarAtivos () {
+  function listarAtivos() {
     // listar funcionarios ativos
     console.log("Listar Ativos");
   }
-  function limparFiltros () {
+  function limparFiltros() {
     // exibir todos os funcionarios
     console.log("Mostrar todos");
   }
 
   return (
     <>
-    <h1>Dashboard</h1>
-    
-    <button onClick={adicionarFuncionario}>+ Adicionar funcionário</button>
+      <h1>Dashboard</h1>
 
-    <button onClick={listarAtivos}>Ver apenas ativos</button>
-    <button onClick={limparFiltros}>Limpar Filtros</button>
+      <button onClick={adicionarFuncionario}>+ Adicionar funcionário</button>
+
+      <button onClick={listarAtivos}>Ver apenas ativos</button>
+      <button onClick={limparFiltros}>Limpar Filtros</button>
+
+      <ul>
+        {data.map(funcionario => (
+          <li key={funcionario.objectID}>
+            id: {funcionario.id} <br />
+            Nome: {funcionario.nome} <br />
+            CPF: {funcionario.cpf}
+            Cargo: {funcionario.cargoId}
+            Atividades: {funcionario.atividades[0].atividadeId}
+          </li>
+        ))}
+      </ul>
+
     </>
   );
 }
